576bb082c575b0d4ccdb57030771396147e5683e
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-04-25 00:45:59 +0000

    added ability to render a hard-coded array of tweets (see app.js)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,27 +20,76 @@ var sample_tweet = {
   "created_at": 1461116232227
 };
 
+const tweetList = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
 // Create tweet:
 
-function createTweet(tweet_info){
+function createTweet(tweet){
   var $tweet = $('<article>').addClass('tweet');
   $tweet.append('<header>');
-  $tweet.append('<p>').find('p').text(tweet_info.content.text);
-  $tweet.append('<hr>');
+  $tweet.append('<p>').find('p').text(tweet.content.text);
   $tweet.append('<footer>');
-  $tweet.find('header').append('<h3>').find('h3').text(`${tweet_info.user.name} aka ${tweet_info.user.handle}`);
-  $tweet.find('header').append('<img>').find('img').attr('src', tweet_info.user.avatars.regular);
-  $tweet.find('footer').append('<p>').addClass('date-tweeted');
+  $tweet.find('header').append('<h3>').find('h3').text(`${tweet.user.name} aka ${tweet.user.handle}`);
+  $tweet.find('header').append('<img>').find('img').attr('src', tweet.user.avatars.regular);
+  $tweet.find('footer').append('<p>').addClass('date-tweeted').find('p').text('date tewwted: date');
+  console.log($tweet[0]);
   return $tweet;
 }
 
-var new_tweet = createTweet(sample_tweet)
-console.log(new_tweet[0]);
+
+function renderTweets(tweetList){
+  for (let tweet of tweetList){
+    $('#tweets-container').append(createTweet(tweet));
+    // console.log(tweet);
+  }
+}
 
 
-// Write tweet to index.html:
 $(document).ready(function(){
-  $('#tweets-container').append(createTweet(sample_tweet));
-
-  
+  renderTweets(tweetList);
 });
---
+++ public/scripts/timeConverter.js
@@ -0,0 +1,7 @@
+// module.exports = 
+
+var theBeginning = new Date(0);
+
+console.log('the beginning=', theBeginning);
+
+var now = theBeginning
