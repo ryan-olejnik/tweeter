afa64ce3fc606011c7ee484f7672c279e1329c94
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-04-26 12:46:43 +0000

    css style updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,66 +1,3 @@
-// var sample_tweet = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": {
-//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//     },
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// };
-
-// const tweetList = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
 // Create tweet:
 
 function createTweet(tweet){
@@ -71,7 +8,7 @@ function createTweet(tweet){
   $tweet.find('header').append('<h3>').find('h3').text(`${tweet.user.name} aka ${tweet.user.handle}`);
   $tweet.find('header').append('<img>').find('img').attr('src', tweet.user.avatars.regular);
   $tweet.find('footer').append('<p>').addClass('date-tweeted').find('p').text(findDaysAgo(tweet.created_at));
-  // create children seperately (with text), and then append them to article so that if additional <p> are added, they are not targeted
+  // CHANGES TO MAKE: create children seperately (with text), and then append them to article so that if additional <p> are added, they are not targeted
   return $tweet;
 }
 
@@ -94,7 +31,6 @@ $(document).ready(function(){
     event.preventDefault();
     // alert('tweet was clicked');
     // console.log(event);
-    $('.new-tweet').toggleClass('hidden');
 
     var tweet = $(this).serialize();
     var tweetLength = $(this).find('textarea').val().length;
@@ -121,6 +57,7 @@ $(document).ready(function(){
       });
     $(this).find('textarea').val('');
   }
+  $('#new-tweet-form').find('textarea').focus();
   });
 
 
@@ -139,8 +76,7 @@ $(document).ready(function(){
 
   //Toggle new-tweet section visibility:
   $('#toggle-newTweet-view').on('click', function() {
-    // alert('button clicked!');
-    $('.new-tweet').toggleClass('hidden');
+    $('.new-tweet').slideToggle(50);
     $('.new-tweet').find('textarea').focus();
 
   });
